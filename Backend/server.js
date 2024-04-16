f825ff5644735e9c98c11e0061e5e9373b82ea12
loictossou2004 <loic.tossou@epitech.eu>
2024-04-16 12:01:36 +0200

    Ajout de la fonction de hash

--- Backend/server.js
+++ Backend/server.js
@@ -7,42 +7,31 @@ const redis = require('redis');
 const crypto = require('crypto');
 dotenv.config();
 
-
+// Permettre les requêtes
 const corsOptions = {
   origin: "*",
   credentials: true,
   optionSuccessStatus: 200,
 };
+
 const app = express();
 const port = 5000;
 
-
+// Connection au redis
 let client = redis.createClient();
 client.connect()
 client.on("connect", function () {
     console.log("Connection Successful!!");
 });
 
-
-const key = Buffer.from(`${process.env.KEY_CRYPTO}`.padEnd(32, '\0'), 'utf8');
-
-function encryptAES(text, key) {
-    const cipher = crypto.createCipheriv('aes-256-cbc', key, Buffer.alloc(16, 0)); 
-    let encrypted = cipher.update(text, 'utf8', 'hex');
-    encrypted += cipher.final('hex');
-    return encrypted;
+// Fonction de hash SHA-256
+function createSHA256Hash(plaintext) {
+    return crypto.createHash('sha256').update(plaintext).digest('hex')
 }
-
-function decryptAES(ciphertext, key) {
-    const decipher = crypto.createDecipheriv('aes-256-cbc', key, Buffer.alloc(16, 0));
-    let decrypted = decipher.update(ciphertext, 'hex', 'utf8');
-    decrypted += decipher.final('utf8');
-    return decrypted;
-}
-
  
 
 async function sendOTPByEmail(email) {
+    // Setting des paramètres de celui qui envoie le mail
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         host: "smtp.gmail.com",
@@ -54,6 +43,7 @@ async function sendOTPByEmail(email) {
         }
     });
 
+    // Génération du code OTP avec des chiffres
     let OTPCode = otpGenerator.generate(6, { 
         digits: true,
         upperCaseAlphabets: false,
@@ -62,6 +52,7 @@ async function sendOTPByEmail(email) {
     });
     console.log('OTP TOTP généré:', OTPCode);
 
+    // Setting des paramètres du mail à envoyer
     let mailOptions = {
         from: process.env.SMTP_MAIL,
         to: email,
@@ -69,16 +60,21 @@ async function sendOTPByEmail(email) {
         text: `Votre code de vérification est : ${OTPCode}`
     };
 
+    // Envoie du mail
     await transporter.sendMail(mailOptions);
     console.log('E-mail envoyé avec succès !');
 
-    const maintenant = new Date();
-    const date = maintenant.toLocaleDateString();
-    const heure = maintenant.toLocaleTimeString();
+    // Récupération de la date et de l'heure
+    const Time = new Date();
+    const date = Time.toLocaleDateString();
+    const heure = Time.toLocaleTimeString();
     const dateTimeString = `${date} ${heure}`;
-    const OTPCrypt = encryptAES(OTPCode, key)
+    
+    // Hash du code envoyé
+    const OTPCrypt = createSHA256Hash(OTPCode)
     
 
+    // Enregistrement dans le serveur redis
     client.set(`${email}`, `Code: ${OTPCrypt}; Date: ${dateTimeString}; attempts: 0`, (err, stu) => {
         if (err) console.log(err);
         else console.log(stu);
@@ -103,22 +99,21 @@ app.get('/verif', async(req, res) => {
     res.send('Hello World!');
 
     const emailOTP = "loic.tossou45@gmail.com"
-    const codeOTPSend = "48305"
+    let codeOTPSend = createSHA256Hash("868549")
 
     let getValueRedis = await client.get(emailOTP)
     const elements = getValueRedis.split(";");
 
-    const GetOTPCodeEncrypt = elements[0].trim().split(" ");
-    const GetOTPCodeDecrypt = decryptAES (GetOTPCodeEncrypt[1], key);
+    let GetOTPCodeEncrypt = elements[0].trim().split(" ");
 
     const attemptsElement = elements[2].trim().split(" ");
     let attemptValue = parseInt(attemptsElement[attemptsElement.length - 1]);
-    console.log(GetOTPCodeDecrypt)
+
     console.log(elements)
     console.log(attemptsElement)
     console.log(attemptValue)
     
-    if (GetOTPCodeDecrypt != codeOTPSend) {
+    if (GetOTPCodeEncrypt[1] !== codeOTPSend) {
         attemptValue++;
         if (attemptValue > 3) {
             console.error("Trop d'attempt tu seras redirigé sur le formulaire d'auth")
@@ -138,26 +133,6 @@ app.get('/verif', async(req, res) => {
 
 }),
 
-app.get('/Next', async(req, res) => {
-    res.send('Hello World!');
-    const sharedSecret = "Your secret"
-
-    client.set("ma_clé", "ma_valeur", 'EX', 10, function(err, reply) {
-        if (err) {
-            console.error('Erreur lors de la définition de la clé:', err);
-        } else {
-            console.log('Clé définie avec succès. Réponse du serveur Redis:', reply);
-        }
-    
-        // Terminer la connexion Redis
-        client.quit();
-    });
-
-    
-}),
-
 app.listen(port, async() => {
-    console.log(`Le serveur est en cours d'exécution sur http://localhost:${port}`);
-
-    
+    console.log(`Le serveur est en cours d'exécution sur http://localhost:${port}`);   
 });
